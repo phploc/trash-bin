58fb38735e4c4afe187827f7c230f7c53c8fb4a2
phploc <32526361+phploc@users.noreply.github.com>
2017-10-21 23:20:19 +0300

    Конвертер json - txt
    Подходит для записи тестов в txt пишет вопрос и ответ на следующей строчке

---
+++ json_to_txt.php
@@ -0,0 +1,17 @@
+<?php
+
+$json_string=file_get_contents('qa.json');
+$normal_string=json_decode($json_string,true);
+foreach($normal_string as $v){
+	static $i=1;
+	$string=$string.'Вопрос '.$i.' :'.$v['q'].';'.PHP_EOL.'Ответ:'.$v['a'].PHP_EOL;
+	$i++;
+}
+mb_convert_variables('Windows-1251','utf-8',$string);
+	file_put_contents('ans.txt',$string);
+	/*
+$r=array(1,2,3,4,5,6,7);
+foreach($r as $v){
+	$s=$s.$v.'<br>';
+	echo '<br>',$s;
+}*/
